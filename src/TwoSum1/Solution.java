db08f365a5b7486117df1663505f4098febddb0e
BogdanKaliuzhniy <kaliuzhnb@gmail.com>
2023-05-01 13:58:59 +0300

    1. Two Sum ver.2

--- src/TwoSum1/Solution.java
+++ src/TwoSum1/Solution.java
@@ -1,13 +1,21 @@
 package TwoSum1;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if ((nums[i] + nums[j]) == target) {
-                    return new int[]{i, j};
-                }
+            int a = nums[i];
+            int b = target - a;
+            if(map.containsKey(b)) {
+                int j = map.get(b);
+             return new int[] {i, j};
             }
+            map.put(a, i);
         }
         return null;
     }
